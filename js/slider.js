af136668303399c79a54c0935f99ee6302f30ba8
Nandana Senarath <nandanasenarath@gmail.com>
2024-06-29 11:21:48 +0530

    landing page creation

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,23 @@
-const myCarouselElement = document.querySelector('#myCarousel')
+document.addEventListener('DOMContentLoaded', function () {
+  const myCarouselElement = document.querySelector('#myCarousel');
+  const carousel = new bootstrap.Carousel(myCarouselElement, {
+    interval: 2000,
+    touch: false
+  });
+});
 
-const carousel = new bootstrap.Carousel(myCarouselElement, {
-  interval: 2000,
-  touch: false
-})
+const sections = document.querySelectorAll('.fade-up');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('fade-up-active');
+      observer.unobserve(entry.target); // Optional: Stop observing once animated
+    }
+  });
+});
+
+sections.forEach(section => {
+  observer.observe(section);
+});
+
